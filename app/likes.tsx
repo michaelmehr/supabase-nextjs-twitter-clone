c9ddaadfc8a107ed398fb38279d5fd85c4797bcd
Michael Mehr <michaelmehr0@gmail.com>
2023-10-28 18:35:11 -0500

    dynamically update UI

--- app/likes.tsx
+++ app/likes.tsx
@@ -3,14 +3,22 @@
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import { useRouter } from "next/navigation"
 
-export default function Likes({ tweet }: { tweet: TweetWithAuthor }) {
+export default function Likes({ tweet, addOptimisticTweet }: { tweet: TweetWithAuthor; addOptimisticTweet: (newTweet: TweetWithAuthor) => void }) {
     const router = useRouter()
     const handleLikes = async () => {
         const supabase = createClientComponentClient<Database>()
         const {data: {user}}= await supabase.auth.getUser()
         if (user) {
             if (tweet.user_has_liked_tweet) {
-                await supabase.from('likes').delete().match({ user_id: user.id, tweet_id: tweet.id })
+                addOptimisticTweet({
+                    ...tweet,
+                    likes: tweet.likes - 1,
+                    user_has_liked_tweet: !tweet.user_has_liked_tweet,
+                })
+                await supabase
+                    .from('likes')
+                    .delete()
+                    .match({ user_id: user.id, tweet_id: tweet.id })
             } else {
                 await supabase
                     .from('likes')
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import AuthButtonServer from './auth-button-server'
 import { redirect } from 'next/navigation'
 import { Database } from '@/lib/database.types'
 import NewTweet from './new-tweet'
-import Likes from './likes'
+import Tweets from './tweets'
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies })
@@ -30,15 +30,7 @@ export default async function Home() {
     <>
       <AuthButtonServer />
       <NewTweet />
-      {tweets?.map(tweet => (
-        <div key={tweet.id}>
-          <p>
-            {tweet.author.name} {tweet.author.username}
-          </p>
-          <p>{tweet.title}</p>
-          <Likes tweet={tweet} />
-        </div>
-      ))}
+      <Tweets tweets={tweets} />
     </>
   )
 }
