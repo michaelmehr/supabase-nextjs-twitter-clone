d93eb0cbe19fa5496ef2c7b3100d058c8efc10b3
Michael Mehr <michaelmehr0@gmail.com>
2023-10-28 17:35:54 -0500

    dynamic like buttons

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import AuthButtonServer from './auth-button-server'
 import { redirect } from 'next/navigation'
 import { Database } from '@/lib/database.types'
 import NewTweet from './new-tweet'
+import Likes from './likes'
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies })
@@ -14,15 +15,29 @@ export default async function Home() {
     redirect('/login')
   }
 
-  const { data: tweets } = await supabase
+  const { data } = await supabase
     .from('tweets')
-    .select('*, profiles(*)')
+    .select('*, profiles(*), likes(*)')
+  
+  const tweets = data?.map(tweet => ({
+    ...tweet,
+    user_has_liked_tweet: tweet.likes.find(like => like.user_id === session.user.id),
+    likes: tweet.likes.length
+  })) ?? [];
 
   return (
     <>
       <AuthButtonServer />
       <NewTweet />
-      <pre>{JSON.stringify(tweets, null, 2)}</pre>
+      {tweets?.map(tweet => (
+        <div key={tweet.id}>
+          <p>
+            {tweet?.profiles?.name} {tweet?.profiles?.username}
+          </p>
+          <p>{tweet.title}</p>
+          <Likes tweet={tweet} />
+        </div>
+      ))}
     </>
   )
 }
--- lib/database.types.ts
+++ lib/database.types.ts
@@ -9,6 +9,68 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      likes: {
+        Row: {
+          created_at: string
+          id: number
+          tweet: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          tweet: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          tweet?: string
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "likes_tweet_fkey"
+            columns: ["tweet"]
+            referencedRelation: "tweets"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "likes_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      profiles: {
+        Row: {
+          avatar_url: string
+          id: string
+          name: string
+          username: string
+        }
+        Insert: {
+          avatar_url: string
+          id: string
+          name: string
+          username: string
+        }
+        Update: {
+          avatar_url?: string
+          id?: string
+          name?: string
+          username?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "profiles_id_fkey"
+            columns: ["id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       tweets: {
         Row: {
           created_at: string
@@ -32,7 +94,7 @@ export interface Database {
           {
             foreignKeyName: "tweets_user_id_fkey"
             columns: ["user_id"]
-            referencedRelation: "users"
+            referencedRelation: "profiles"
             referencedColumns: ["id"]
           }
         ]
